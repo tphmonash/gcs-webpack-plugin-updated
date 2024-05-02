5e04c84eee43305e891ee15815b9130df5ccc211
Tyler Pedersen-Hor <tyler.pedersen-hor@monash.edu>
2024-05-02 16:37:00 +1000

    Update to working build with major package updates

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -1,14 +1,14 @@
 // NOTE: Taken from https://webpack.github.io/docs/usage.html
-const path = require('path');
-const WebpackGoogleCloudStoragePlugin = require('../dist/webpack-google-cloud-storage-plugin.js');
+const path = require("path");
+const WebpackGoogleCloudStoragePlugin = require("../dist/webpack-google-cloud-storage-plugin");
 
 module.exports = {
   context: __dirname,
-  entry: './src/app.js',
+  entry: "./src/app.js",
 
   output: {
-    path: './bin',
-    filename: 'app.bundle.js',
+    path: "./bin",
+    filename: "app.bundle.js",
   },
 
   module: {
@@ -17,9 +17,9 @@ module.exports = {
         test: /\.js$/,
         include: /src/,
         exclude: /node_modules/,
-        loader: 'babel',
+        loader: "babel",
         query: {
-          presets: ['es2015'],
+          presets: ["es2015"],
         },
       },
     ],
@@ -27,18 +27,18 @@ module.exports = {
   debug: true,
   plugins: [
     new WebpackGoogleCloudStoragePlugin({
-      directory: './src',
+      directory: "./src",
       // NOTE: Array of filename strings (or regex) to include in the uploading process
-      include: ['.js'],
+      include: [".js"],
       // NOTE: Array of filename strings (or regex) to exclude in the uploading process
-      exclude: ['cats.js'],
+      exclude: ["cats.js"],
       // NOTE: Options passed directly to
       // Google cloud Node Storage client.
       // This is mostly authentication-wise.
       // For more information:
       // https://github.com/GoogleCloudPlatform/google-cloud-node/tree/master/packages/storage#authentication
       storageOptions: {
-        projectId: 'grape-spaceship-123',
+        projectId: "grape-spaceship-123",
         // keyFilename: '/path/to/keyfile.json'
         // keyFileName: './examples/my-credentials.json',
         // key: 'mykey',
@@ -49,7 +49,7 @@ module.exports = {
       // regarding uploading.
       uploadOptions: {
         // Where to upload files
-        bucketName: 'my-bucket-name',
+        bucketName: "my-bucket-name",
         // Whether to create the bucket if it doesn't exist already
         // default: false
         // NOTE: Currently not tested and most likely not working.
@@ -57,9 +57,7 @@ module.exports = {
         // NOTE: Prefix to add in the bucket file path.
         // E.g: app.js => assets/v1/app.js,
         // file is an object with { name:, path: }.
-        destinationNameFn: file =>
-           path.join('assets', file.path)
-        ,
+        destinationNameFn: (file) => path.join("assets", file.path),
       },
     }),
   ],
--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,9 @@
 // eslint-disable-next-line import/prefer-default-export
 export function pick(object, paths) {
-  const obj = {};
-  for (const path of paths) {
-    if (object[path]) {
-      obj[path] = object[path];
+  return paths.reduce((accumulator, path) => {
+    if (Object.prototype.hasOwnProperty.call(object, path)) {
+      accumulator[path] = object[path];
     }
-  }
-  return obj;
+    return accumulator;
+  }, {});
 }
